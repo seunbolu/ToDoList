fccddae46e47756ba5af2a511d980212c3432af6
Oluwaseun Atoyebi <seunbolu86@gmail.com>
2020-11-19 19:56:35 -0500

    all model added

---
+++ ToDoList/Models/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class Status
+    {
+        public string StausId { get; set; }
+        public string Name { get; set; }
+    }
+}
--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -22,6 +22,11 @@ namespace ToDoList
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting(Options =>
+            {
+                Options.LowercaseUrls = true;
+                Options.AppendTrailingSlash = true;
+            });
             services.AddControllersWithViews();
         }
 
